18fbb8e92305d5d64a19f059dcd45344e042d313
armarram <arram.s1998@gmail.com>
2023-05-09 21:05:50 +0700

    - fix footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
             <div className="container text-center text-md-start mt-5">
               <div className="row mt-3">
                 <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Holiday Dreams</h6>
+                  <h6 className="text-uppercase fw-bold">Koh chang club</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
                     style={{
