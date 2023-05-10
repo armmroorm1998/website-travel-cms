3471fca3a17cf9455009dde6b29dec5c2aa0e1f6
armarram <arram.s1998@gmail.com>
2023-05-10 09:51:12 +0700

    - delete tour description

--- src/Components/TourDescription/TourDescription.js
+++ src/Components/TourDescription/TourDescription.js
@@ -1,55 +1,15 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import classes from "./TourDescription.module.css";
-
 const TourDescription = ({ tour }) => {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
+  // const options = {
+  //   weekday: "long",
+  //   year: "numeric",
+  //   month: "long",
+  //   day: "numeric",
+  // };
 
   return (
-    <section className={classes.description}>
-      <Container>
-        <Row className="align-items-center">
-          <Col lg={6}>
-            <div className={classes["description-content"]}>
-              <h2 className="section-heading-sm">รายละเอียดทัวร์</h2>
-              <ul>
-                <li className={classes.price}>
-                  <span>PRICE : </span> ฿{tour.price}
-                </li>
-                <li>
-                  <span>NEXT DATE :</span>
-                  {new Date(tour.startDates).toLocaleDateString(
-                    "en-US",
-                    options
-                  )}
-                </li>
-
-                <li>
-                  <span>MAX PARTICIPANTS : </span> {tour.maxGroupSize} ท่าน
-                </li>
-                <li>
-                  <span>Duration : </span> {tour.duration} วัน
-                </li>
-                <li>
-                  <span>DIFFICULTY : </span> {tour.dinner}
-                </li>
-                <li>
-                  <span>RATING : </span> {tour.ratingsAverage} / 5
-                </li>
-
-                <li>
-                  <span>Description : </span> {tour.description}
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <section>
+      <div></div>
     </section>
   );
 };
